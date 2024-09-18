67faad31077b38c656a3b1cf8ba6d8e14a77cc6f
marvi2011 <marvi661@hotmail.com>
2024-09-18 10:46:53 -0300

    pude hacer que la api me mande la informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import Formulario from "./componentes/Formulario"
-
+import Formulario from "./componentes/Formulario";
+import { useEffect } from "react";
 
 function App() {
-  
-
+  useEffect(() => {consultaApi()}, []);
+const consultaApi = async ()=>{
+const respuestaApi= await fetch("https://newsdata.io/api/1/latest?apikey=pub_53787c366b1325ce0510477ff1fcc77bc1e31&category=politics&country=bd")
+const datos= await respuestaApi.json()
+console.log(datos)
+}
   return (
     <main className="container mt-3">
-     <h1 className="text-center text-primary mb-3">Noticias</h1>
-     <Formulario></Formulario>
+      <h1 className="text-center text-primary mb-3">Noticias</h1>
+      <Formulario></Formulario>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
