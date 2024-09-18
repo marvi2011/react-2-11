226ea67a30f53cd8dcc752b84890404eebbf6a55
marvi2011 <marvi661@hotmail.com>
2024-09-18 09:34:19 -0300

    limpia la estructura del proyect

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Formulario from "./componentes/Formulario"
+
+
+function App() {
+  
+
+  return (
+    <main className="container mt-3">
+     <h1 className="text-center text-primary">Noticias</h1>
+     <Formulario></Formulario>
+    </main>
+  )
+}
+
+export default App
